001cbbf19961513664242891716685dc29ab574e
anennumen <pawelwirth33@gmail.com>
2023-09-26 08:28:35 +0200

    wzór

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function sumailoczynu(Liczba1=1, Liczba2=2 , Liczba3=3 , Liczba4=4 ,Liczba5=5,Liczba6=6 ){
+letwynik =Liczba1*Liczba2*Liczba3*Liczba4*Liczba5*Liczba6
+console.log(wynik) 
+}
+sumailoczynu();
